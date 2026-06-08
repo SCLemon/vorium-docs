70eb39dd795cc0d3d7aa1539481246b7671a5f2c
SC1314520 <sclemon1013@gmail.com>
2026-06-08 21:35:36 +0800

    s

---
+++ public/demo-code/CodeEditor/step4.vue
@@ -0,0 +1,85 @@
+<!-- Example rendered above -->
+<template>
+    <div class="set-wrapper">
+        <div class="set-title">Code Editor</div>
+        <div class="set-demo-box-wrapper">
+            <div class="set-demo-box">
+                <VCodeEditor v-model="code" :monaco="monaco"></VCodeEditor>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+import { VCodeEditor } from 'vorium-ui'
+import 'vorium-ui/dist/vorium-ui.css'
+
+// Required Sources
+import * as monaco from 'monaco-editor'
+import 'monaco-editor/min/vs/editor/editor.main.css'
+
+const codeStr = 
+`
+function becomeSuccessful() {
+    const skills = [
+        "Learn",
+        "Practice",
+        "Build"
+    ];
+
+    skills.forEach(skill => {
+        console.log(skill);
+    });
+
+    console.log("Repeat until successful.");
+}
+
+becomeSuccessful();
+`
+
+export default {
+    name: 'CodeEditor',
+    components:{
+        VCodeEditor
+    },
+    setup(){
+        let code = ref(codeStr);
+
+        return {
+            code, monaco
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .set-wrapper{
+        padding-left: 30px;
+        padding-right: 30px;
+        box-sizing: border-box;
+    }
+    .set-title{
+        color: white;
+        font-size: 18px;
+        margin-top: 30px;
+        margin-bottom: 30px;
+    }
+    .set-demo-box-wrapper{
+        width: 100%;
+        padding-top: 30px;
+        padding-bottom: 30px;
+        padding-left: 10px;
+        padding-right: 10px;
+        border: 1px solid rgba(255,255,255,0.2);
+        box-sizing: border-box;
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+    .set-demo-box{
+        width: 45%;
+        height: 500px;
+    }
+</style>
